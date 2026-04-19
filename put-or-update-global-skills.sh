f3a0d009364103353f5e97143067870cced500ab
Diego Lirio <diegolirio.dl@gmail.com>
2026-04-19 10:44:48 -0300

    fix-sccript-to-copy

--- put-or-update-global-skills.sh
+++ put-or-update-global-skills.sh
@@ -1,18 +1,36 @@
 
 echo '========= COPILOT =============='
-mkdir ~/.copilot/
+mkdir -p ~/.copilot/skills/
 cp -r ./skills/* ~/.copilot/skills/
 cp -r ./skills/setup-kotlin-spring/claude/skills/* ~/.copilot/skills/
 echo '========= COPILOT Copied ======='
 
 echo '========= CODEX =============='
-mkdir ~/.codex/
+mkdir -p ~/.codex/skills/
 cp -r ./skills/* ~/.codex/skills/
 cp -r ./skills/setup-kotlin-spring/claude/skills/* ~/.codex/skills/
-echo '========= COPILOT Copied ======='
+echo '========= CODEX Copied ======='
+
+echo '========= CLAUDE =============='
+mkdir -p ~/.claude/skills/
+cp -r ./skills/* ~/.claude/skills/
+cp -r ./skills/setup-kotlin-spring/claude/skills/* ~/.claude/skills/
+echo '========= CLAUDE Copied ======='
+
+echo '========= CURSOR =============='
+mkdir -p ~/.cursor/skills/
+cp -r ./skills/* ~/.cursor/skills/
+cp -r ./skills/setup-kotlin-spring/claude/skills/* ~/.cursor/skills/
+echo '========= CURSOR Copied ======='
 
 echo '========= COPILOT skills =============='
 ls -la ~/.copilot/skills
 
 echo '========= CODEX skills =============='
 ls -la ~/.codex/skills
+
+echo '========= CLAUDE skills =============='
+ls -la ~/.claude/skills
+
+echo '========= CURSOR skills =============='
+ls -la ~/.cursor/skills
